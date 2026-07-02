db7ddcac0ffc52b592a543b3e2227e35cf893773
avirajkmar848-png <avirajkmar848@gmail.com>
2026-07-02 13:52:46 +0530

    Add: last-stone-weight - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int>pq;
+        for(auto x:stones){
+            pq.push(x);
+        }
+        while(pq.size()>1){
+           int x=pq.top();
+            pq.pop();
+           int y=pq.top();
+           pq.pop();
+           if(x==y)continue;
+           else{
+            pq.push(x-y);
+           }
+        } pq.push(0);
+        return pq.top();
+    }
+};
