8d008cccbf89cc3b336d7a371d8ad951320ec9fb
avirajkmar848-png <avirajkmar848@gmail.com>
2026-07-08 18:37:00 +0530

    Add: generate-parentheses - submission-1

---
+++ Data Structures & Algorithms/generate-parentheses/submission-1.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+bool isvalid(string &s){
+    stack<char>st;
+    for(int i=0;i<s.size();i++){
+        if(s[i]=='(')st.push(s[i]);
+        else{
+            if(!st.empty()&&st.top()=='(')st.pop();
+            else st.push(s[i]);
+        }
+    }
+    if(st.empty())return true;
+    return false;
+}
+void helper(int n,int x,int y,vector<string>&ans,string &s){
+    if(s.size()==2*n){
+        if(isvalid(s)) ans.push_back(s);
+        return;
+    }
+    if(x<n){
+        s.push_back('(');
+        helper(n,x+1,y,ans,s);
+        s.pop_back();}
+    if(y<n) {
+        s.push_back(')');
+        helper(n,x,y+1,ans,s);
+        s.pop_back();}
+}
+    vector<string> generateParenthesis(int n) {
+        vector<string>ans;
+        string s="";
+        helper(n,0,0,ans,s);
+        return ans;
+    }
+};
